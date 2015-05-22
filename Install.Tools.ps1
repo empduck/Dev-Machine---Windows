f5853c5eb25259b6c52d8cfd4c0c5c931943aa5f
empduck <josephwbell@gmail.com>
2015-05-22 10:12:24 -0400

    Quote Correction

--- Install.Tools.ps1
+++ Install.Tools.ps1
@@ -3,7 +3,7 @@ cinst -y githubforwindows
 cinst -y node.js
 cinst -y notepadplusplus
 cinst -y 7zip
-cinst -y visualstudio2013ultimate  -InstallArguments "/Features:'Blend LightSwitch VCMFCLibraries OfficeDeveloperTools SQL WebTools Win8SDK SilverLightDeveloperKit WindowsPhone80'
+cinst -y visualstudio2013ultimate  -InstallArguments "/Features:'Blend LightSwitch VCMFCLibraries OfficeDeveloperTools SQL WebTools Win8SDK SilverLightDeveloperKit WindowsPhone80'"
 cinst -y mssqlserver2014express
 cinst -y mssqlservermanagementstudio2014express
 cinst -y sublimetext3
